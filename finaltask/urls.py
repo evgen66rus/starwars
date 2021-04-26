d23a6fe89d671117552258ce3b77de8ff28af485
Evgeny <evgen.alyoshin@gmail.com>
2021-04-26 17:09:17 +0500

    architecture design

---
+++ finaltask/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+from django.conf.urls import url
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('character/', views.character, name='character'),
+    path('planets/', views.planets, name='planets'),
+    path('planets/<int:pk>/', views.planet_detail_view, name='planet-detail')
+]
--- finaltask/views.py
+++ finaltask/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from .models import Character, Planet
+from django.views import generic
 
-# Create your views here.
+def index(request):
+    return render(request, 'base_generic.html')
+
+def character(request):
+    person = Character.objects.all()
+    context = {
+        'person': person
+    }
+    return render(request, 'character.html', context=context)
+
+def planets(request):
+    residents = []
+    planets = Planet.objects.all()
+    context = {
+        'planets': planets,
+    }
+    return render(request, 'planets.html', context=context)
+
+def planet_detail_view(request,pk):
+    try:
+        planet_id=Planet.objects.get(pk=pk)
+    except Planet.DoesNotExist:
+        raise Http404("Planet does not exist")
+    characters = Character.objects.filter(homeworld__name=planet_id)
+    context = {
+        'planet_id': planet_id,
+        'characters': characters,
+    }
+    return render(request, 'planet-detail.html', context=context)
