a06720f96827db613b8bfb309392e3de11e5e515
Evgeny <evgen.alyoshin@gmail.com>
2021-04-27 09:01:11 +0500

    update data
    added harakiri parameter to uwsgi, install requests with docker, removed unnecessary migrations

--- finaltask/urls.py
+++ finaltask/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('character/', views.character, name='character'),
     path('planets/', views.planets, name='planets'),
-    path('planets/<int:pk>/', views.planet_detail_view, name='planet-detail')
+    path('planets/<int:pk>/', views.planet_detail_view, name='planet-detail'),
+    path('update-data/', views.update_data, name='update-data'),
 ]
--- finaltask/views.py
+++ finaltask/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Character, Planet
 from django.views import generic
+import requests, json
+from django.http import HttpResponse
 
 def index(request):
     return render(request, 'base_generic.html')
@@ -31,3 +33,40 @@ def planet_detail_view(request,pk):
         'characters': characters,
     }
     return render(request, 'planet-detail.html', context=context)
+
+def update_data(request):
+    # url = 'https://swapi.dev/api/people/1/'
+    # zapros = requests.get(url)
+    # zapros = zapros.json()
+
+    url = 'https://swapi.dev/api'
+    url_people = url + '/people'
+    url_planets = url + '/planets'
+
+    planets_data = requests.get(url_planets)
+    planets_data = planets_data.json()['results']
+
+    for parameters in planets_data:
+        planet_name = parameters['name']
+        planet_gravity = parameters['gravity']
+        planet_climate = parameters['climate']
+        planet_residents_urls = parameters['residents']
+        char_list = []
+        char_details = {}
+        planet_data = {}
+        for char_url in planet_residents_urls:
+            char_raw_data = requests.get(char_url)
+            char_raw_data = char_raw_data.json()
+            char_name = char_raw_data['name']
+            char_list.append(char_name)
+            
+            char_gender = char_raw_data['gender']
+            char_details = {'name': char_name, 'homeworld': planet_name, 'gender': char_gender}
+            #print(char_details)
+        planet_data = {'name': planet_name, 'gravity': planet_gravity, 'climate': planet_climate, 'residents': char_list}
+        #print(planet_data)
+
+
+
+    html = "<html><body><pre>Data: %s.</pre></body></html>" % json.dumps(char_details, indent=2)
+    return HttpResponse(html)
