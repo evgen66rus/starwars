e10e5107698344070261cba50235bc4dced12b73
Evgeny <evgen.alyoshin@gmail.com>
2021-08-09 23:28:08 +0500

    testing migrations

--- finaltask/admin.py
+++ finaltask/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Character, Planet
+from .models import Character, Planet, Notes
 
 admin.site.register(Character)
 admin.site.register(Planet)
+admin.site.register(Notes)
 
 class PlanetAdmin(admin.ModelAdmin):
    list_display = ("name")
@@ -11,3 +12,6 @@ class PlanetAdmin(admin.ModelAdmin):
 class CharacterAdmin(admin.ModelAdmin):
    list_display = ("name")
    search_fields = ("name__startwith", )
+
+class NotesAdmin(admin.ModelAdmin):
+   list_display = ("name")
